873e5eeed1de5b70fb93713ff86cd3a900e9213b
Mata <Edson.Mata@Honeywell.com>
2022-03-10 10:05:17 -0600

    letter checker functionality done...

--- letterChecker.py
+++ letterChecker.py
@@ -1,5 +1,21 @@
 #here is where we determinate how many letters are correct in the word the user provides us comparing it with the word we randomly select
 
 def correctLetters(guess, word):
-    print(word)
-    print(guess)
+    guessL = list(guess)
+    wordL = list(word)
+
+    correctNumnber = 0
+
+    print(guessL)
+    print(wordL)
+
+    for x in guessL:
+        for y in wordL:
+            if x == y:
+                print("Letter has been found: ", x)
+                if guessL.index(x) == wordL.index(y):
+                    print("The found letter is in the correct position")
+                else:
+                    print("The found letter is in other position")
+                break 
+    
